b4b8914b5de6ab307a33dc99fc9e48aff67bf915
Devamardeep Hayatpur <devamar_10@hotmail.com>
2017-02-28 15:47:43 -0500

    Revert "Freeze animation on mobile devices"
    This reverts commit 20b49f6796116518c6fe77e022224382dae83702.

--- js/main.js
+++ js/main.js
@@ -1,48 +1,49 @@
 var global_comp = []
 
 function setup() {
-    //P5 Init
-    noCanvas();
-    frameRate(30);
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        noLoop()
-    }
-    var svg_width = 600
-    var svg_height = 600
-    var centerx = 250
-    var centery = 250
-    
-    //D3 Init
-    var container = d3.select('body');
-    var svg = container.append('svg')
-        .attr('width', svg_width)
-        .attr('height', svg_height)
-        .attr('id', 'main-svg')
+  //P5 Init
+  noCanvas();
+  frameRate(30);
+  var svg_width = 600
+  var svg_height = 600
+  var centerx = 250
+  var centery = 250
 
-    //Circular Noises
-    cNoise = new CircularNoise(0.2);
-    cNoiseNuclei = new CircularNoise(0.1);
+  //D3 Init
+  var container = d3.select('body');
+  var svg = container.append('svg')
+    .attr('width', svg_width)
+    .attr('height', svg_height)
+    .attr('id', 'main-svg')
 
-    //Component Initialization
-    cell_membrane = new Blob(svg, 2, 200, 200, 5, 10, centerx, centery, cNoise, '#831973', 'cell_stroke', 'Cell Membrane');
-    cytoplasm = new Blob(svg, 2, 190, 190, 5, 10, centerx, centery, cNoise, '#331a50', 'cell', 'Cytoplasm');
+  //Circular Noises
+  cNoise = new CircularNoise(0.2);
+  cNoiseNuclei = new CircularNoise(0.1);
 
-    nuclear_membrane = new Blob(svg, 2, 70, 70, 0, 5, centerx, centery, cNoiseNuclei, '#c0392b', 'nucleus_stroke', 'Nuclear Membrane');
-    nucleus = new Blob(svg, 2, 65, 65, 0, 5, centerx, centery, cNoiseNuclei, '#e66457', 'nucleus', 'Nucleus');
 
-    nuclei = new Blob(svg, 2, 40, 40, 5, 5, centerx, centery + 10, cNoiseNuclei, '#f1c40f', 'nuclei', 'Nuclei');
+  //CSS Dropshadow
 
-    ribosomes = new Ribosomes(svg, 1, 3, 5, centerx + 50, centery + 90, 30, 30, '#9e71d2', 'ribo', 'Ribosomes');
 
-    lysosome = new Lysosome(svg, 15, 3, 160, 340, '#cb58bf', '#7f377b', 'lysosome', 'Lysosome')
+  //Component Initialization
+  cell_membrane = new Blob(svg, 2, 200, 200, 5, 10, centerx, centery, cNoise, '#831973', 'cell_stroke', 'Cell Membrane');
+  cytoplasm = new Blob(svg, 2, 190, 190, 5, 10, centerx, centery, cNoise, '#331a50', 'cell', 'Cytoplasm');
+
+  nuclear_membrane = new Blob(svg, 2, 70, 70, 0, 5, centerx, centery, cNoiseNuclei, '#c0392b', 'nucleus_stroke', 'Nuclear Membrane');
+  nucleus = new Blob(svg, 2, 65, 65, 0, 5, centerx, centery, cNoiseNuclei, '#e66457', 'nucleus', 'Nucleus');
+
+  nuclei = new Blob(svg, 2, 40, 40, 5, 5, centerx, centery + 10, cNoiseNuclei, '#f1c40f', 'nuclei', 'Nuclei');
+
+  ribosomes = new Ribosomes(svg, 1, 3, 5, centerx + 50, centery + 90, 30, 30, '#9e71d2', 'ribo', 'Ribosomes');
+
+  lysosome = new Lysosome(svg, 15, 3, 160, 340, '#cb58bf', '#7f377b', 'lysosome', 'Lysosome')
 }
 
 function draw() {
-    cell_membrane.draw();
-    cytoplasm.draw();
-    nuclear_membrane.draw();
-    nucleus.draw();
-    nuclei.draw();
-    ribosomes.draw();
-    lysosome.draw();
+  cell_membrane.draw();
+  cytoplasm.draw();
+  nuclear_membrane.draw();
+  nucleus.draw();
+  nuclei.draw();
+  ribosomes.draw();
+  lysosome.draw();
 }
