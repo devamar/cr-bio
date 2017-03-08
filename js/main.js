4ffc9dacf8e6c514d90592a22c22f2c06c601a86
Devamardeep Hayatpur <devamar_10@hotmail.com>
2017-03-07 20:55:54 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,9 +8,10 @@ function setup() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         noLoop()
     }
-    */
+
     button = createButton('reset')
     button.mousePressed(resetSketch)
+        */
     resetSketch()
 }
 
